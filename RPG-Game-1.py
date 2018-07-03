9fe4ce72b87582cc3fedd437159c9a60fd64478a
Melissa Cantu <meli_cantu12@hotmail.com>
2018-07-03 10:48:24 -0500

    almost done step 7

--- RPG-Game-1.py
+++ RPG-Game-1.py
@@ -7,60 +7,71 @@
 # 3. flee
 
 class Character:
-    def __init__(self, health, power):
-         self.health = health
-         self.power = power
+    def __init__(self, name, health, power):
+        self.name = name
+        self.health = health
+        self.power = power
+    
+    def alive(self):
+        if self.health > 0:
+            return True
+    
+    def attack(self, enemy):
+        enemy.health -= self.power
+        self.health -= self.power
+        if self.name == "Hero":
+            print("You do {} damage to the goblin.".format(self.power))
+        if not enemy.alive():
+            print("The goblin is dead")
+        elif enemy.name == "Hero":
+            print("The {} does {} damage to you.".format(self.name, self.power))
+            if not enemy.alive():
+                print("You are dead.")
+
+    def print_status(self):
+        #print("You have {} health and {} power.".format(self.health, self.power))
+        print("The {} have {} health and {} power.".format(self.name, self.health, self.power))
 
 class Hero(Character):
-    def __init__(self, health, power)
-        self.health = 10
-        self.power = 5
-    def attack(self, goblin):
-        goblin.health -= self.power
-        if goblin.health <= 0
-            print("the goblin is dead")
-    def print_status(self):
-        print("You have {} health and {} power.".format(hero_health, hero_power))
+    def __init__(self, name, power, health):
+        super().__init__(name, power, health)
 
-class Goblin(Character):      
-    def __init__(self, health, power)
-        self.health = 6
-        self.power = 2
-    def attack(self, hero):
-        hero.health -= self.power
-        if hero.health <= 0:
-            print("You are dead")
-    def print_status(self):
-        print("The goblin has {} health and {} power.".format(goblin_health, goblin_power))
-
-# melissa = Hero(10,2)
-# veronica = Goblin(6,3)
-
-# melissa.attack(veronica)
+class Goblin(Character):
+    def __init__(self, name, power, health):
+        super().__init__(name, power, health)
 
 def main():
-    while goblin.alive() and hero.alive():
+    hero = Hero("Hero", 10, 5)
+    enemy = Goblin("Goblin", 6, 2)
+    # fight(hero, goblin)
+
+    
+    while enemy.alive() and hero.alive():
+        hero.print_status()
+        enemy.print_status()
         print()
         print("What do you want to do?")
         print("1. fight goblin")
         print("2. do nothing")
         print("3. flee")
         print("> ", end=' ')
-        raw_input = input()
-        if raw_input == "1":
+        inp = input()
+        if inp == "1":
             # Hero attacks goblin
-            hero.attack(goblin)
-        elif raw_input == "2":
+            hero.attack(enemy)
+        elif inp == "2":
             pass
-        elif raw_input == "3":
+        elif inp == "3":
             print("Goodbye.")
             break
         else:
-            print("Invalid input {}".format(raw_input))
+            print("Invalid input {}".format(inp))
 
-        if goblin_health > 0:
+        if enemy.alive():
             # Goblin attacks hero
-            goblin.attack(hero)
-                print("You are dead.")
+            enemy.attack(hero)
+            hero.print_status()
+            enemy.print_status()
+    # 
 
 main()
